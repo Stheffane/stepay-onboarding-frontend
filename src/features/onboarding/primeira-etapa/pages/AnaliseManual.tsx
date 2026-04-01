1855cdb17c3b4d47eed8f7d6647c9da1cf7b7340
stheffane.nunes <stheffane.tr@gmail.com>
2026-04-01 17:41:32 -0300

    feat: create to new stack and contexts for analiseManual

--- src/features/onboarding/primeira-etapa/pages/AnaliseManual.tsx
+++ src/features/onboarding/primeira-etapa/pages/AnaliseManual.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import { Container, Typography, Box } from "@mui/material";
+import Footer from "../../../../shared/components/footer/Footer";
 
-const AnaliseManual: React.FC = () => {
+export default function AnaliseManualPage() {
   return (
-    <div>
-      <h1>Análise Manual</h1>
-      {/* Conteúdo da página */}
-    </div>
-  );
-};
+    <Container maxWidth="sm">
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        textAlign="center"
+        pt={6}
+        gap={2}
+      >
 
-export default AnaliseManual;
+        <Typography variant="h4">
+          Estamos avaliando seu pedido!
+        </Typography>
+
+        <Typography variant="h6" color="text.secondary">
+          Agora faremos uma análise da sua solicitação e enviaremos um e-mail
+          te informando o status dela. ✌️
+        </Typography>
+
+        <Typography variant="h5" mt={2}>
+          Confira sua caixa de entrada. Até mais!
+        </Typography>
+
+        <Typography variant="body1" color="text.secondary">
+          (Confira também no spam, caso não encontre nada na caixa de entrada)
+        </Typography>
+      </Box>
+
+      <Footer
+        rightButton="finalizar"
+        rightButtonLink="https://github.com/Stheffane/stepay-onboarding-frontend"
+      />
+    </Container>
+  );
+}
