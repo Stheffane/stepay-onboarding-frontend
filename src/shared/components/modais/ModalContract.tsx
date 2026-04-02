461d9f5fc5a72424552050f4cc1a79c7108c6982
stheffane.nunes <stheffane.tr@gmail.com>
2026-04-02 15:14:16 -0300

    chore ajust in modais components

--- src/shared/components/modais/ModalContract.tsx
+++ src/shared/components/modais/ModalContract.tsx
@@ -11,9 +11,10 @@ import { Content, Text, Span } from "./styles";
 type ModalPoliticaProps = {
   open: boolean;
   onClose: () => void;
+  title?: string;
 };
 
-export function ModalPolitica({ open, onClose }: ModalPoliticaProps) {
+export function ModalContract({ open, onClose, title }: ModalPoliticaProps) {
   return (
     <Dialog
       open={open}
@@ -24,7 +25,7 @@ export function ModalPolitica({ open, onClose }: ModalPoliticaProps) {
       <DialogTitle
         sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}
       >
-        Política de Privacidade
+        {title || "Política de Privacidade"}
 
         <IconButton onClick={onClose}>
           <CloseIcon />
@@ -35,7 +36,7 @@ export function ModalPolitica({ open, onClose }: ModalPoliticaProps) {
         <Content>
           <Text>
             <Span>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempor metus mi, in porttitor ligula blandit at. Pellentesque malesuada, sapien a bibendum bibendum, ipsum eros elementum sapien, eget cursus mauris nunc nec mauris. Etiam dignissim nibh a leo egestas, id consectetur nisi scelerisque. Etiam quis libero at tellus elementum maximus id ac tellus. Duis quis massa sed leo mattis dapibus. Aenean rutrum semper purus vel volutpat. Aenean porttitor sagittis lacus, eget eleifend nunc.
             </Span>
           </Text>
 
@@ -57,13 +58,27 @@ export function ModalPolitica({ open, onClose }: ModalPoliticaProps) {
             </h3>
 
             <Text>
-              Donec varius ullamcorper risus, sed ullamcorper odio lobortis a...
+              Suspendisse potenti. Donec ut mattis dolor, at hendrerit nulla. Donec consequat viverra orci, nec sollicitudin nisl tristique et. Quisque at libero tortor. Quisque non tempus augue, eget porta nulla. Duis pulvinar urna mauris, maximus ornare ligula sollicitudin at.
             </Text>
 
             <h3>-Lorem Ipsum</h3>
 
             <Text>
               Morbi ac suscipit enim, in pulvinar odio...
+            </Text>
+
+            <Text>
+              Nullam ac dapibus lacus. Integer bibendum sit amet orci in fermentum. Proin vehicula lorem sed arcu volutpat porttitor. Aenean interdum quis nulla non placerat. In aliquam tellus lectus, eget lobortis purus tempus eu. Nam vestibulum massa a orci aliquam, vitae convallis diam ultricies. In vitae massa eget sapien varius lobortis. Curabitur ac ultricies enim, ac fermentum odio. Mauris eu libero in odio cursus molestie a eu dui. Vestibulum molestie, diam a pulvinar convallis, lacus nulla ornare dolor, ut placerat metus mauris vitae metus.
+            </Text>
+
+            <h3>-Lorem Ipsum</h3>
+
+            <Text>
+              Sed mi diam, eleifend et cursus non, varius sed tortor.
+            </Text>
+
+            <Text>
+              Morbi vitae odio et enim ultricies aliquet sed sit amet felis. Donec a tincidunt ante, sed ultricies urna. Mauris luctus, quam at vestibulum lobortis, ex enim ultricies turpis, a malesuada orci est ut est. Nam consectetur in ligula ac viverra. Mauris eleifend ornare arcu, eu semper neque lacinia at. Ut posuere ex ex, eu maximus augue aliquam vel. Vivamus vitae cursus odio, vel venenatis est. Pellentesque fringilla eros ante, porttitor scelerisque nunc venenatis quis. Ut fermentum consequat congue. Nam vulputate elit ac sollicitudin tempor.
             </Text>
           </Text>
         </Content>
--- src/shared/models/dados.model.ts
+++ src/shared/models/dados.model.ts
@@ -1,6 +1,9 @@
-import type { DadosPessoaisForm, DetalhesPedidoForm } from "../../features/onboarding/primeira-etapa/schemas";
-import type { SimulacaoForm } from "../../features/onboarding/primeira-etapa/schemas/simulacao.schema";
-
+import type {
+  DadosPessoaisForm,
+  DetalhesPedidoForm,
+  SimulacaoForm,
+  ConfirmacaoForm
+} from "../../features/onboarding/primeira-etapa/schemas";
 export interface DadosModel {
   id?: string;
   etapaAtual?: number;
@@ -8,4 +11,5 @@ export interface DadosModel {
   dadosPessoais?: DadosPessoaisForm;
   detalhesPedido?: DetalhesPedidoForm;
   simulacao?: SimulacaoForm;
+  confirmacao?: ConfirmacaoForm;
 }
