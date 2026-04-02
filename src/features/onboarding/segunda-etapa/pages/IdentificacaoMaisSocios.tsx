c98dc5ab0c50e939b6212f90e1fa311a3d16f43d
stheffane.nunes <stheffane.tr@gmail.com>
2026-04-02 16:12:57 -0300

    feat: create to new stack using IdentificacaoBase

--- src/features/onboarding/segunda-etapa/pages/IdentificacaoMaisSocios.tsx
+++ src/features/onboarding/segunda-etapa/pages/IdentificacaoMaisSocios.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import { useNavigate } from "react-router-dom";
+import { PATHS } from "../../../../app/router/paths";
+import { IdentificacaoBase } from "../components/IdentificationOfMorePartners";
 
-const IdentificacaoMaisSocios: React.FC = () => {
+export default function IdentificacaoMaisSociosPage() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Identificação Mais Sócios</h1>
-      {/* Conteúdo da página */}
-    </div>
+    <IdentificacaoBase
+      title="Identificamos que sua empresa possui mais sócios no quadro societário."
+      subtitle="Envie o link para seu(s) sócio(s) realizar a confirmação de dados."
+      onBack={() => navigate(-1)}
+      onNext={() => navigate(PATHS.SEGUNDA_ETAPA.PROXIMOS_PASSOS.SOCIOS)}
+    />
   );
-};
-
-export default IdentificacaoMaisSocios;
+}
