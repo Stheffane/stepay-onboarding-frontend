569083d641ff88a91a30985b26852aee05505b84
stheffane.nunes <stheffane.tr@gmail.com>
2026-04-05 19:28:55 -0300

    chore: add DadosBancariosModel to DadosModel

--- src/shared/models/dados.model.ts
+++ src/shared/models/dados.model.ts
@@ -4,14 +4,23 @@ import type {
   SimulacaoForm,
   ConfirmacaoForm
 } from "../../features/onboarding/primeira-etapa/schemas";
+
+export interface DadosBancariosModel {
+  banco?: string;
+  codigoBanco?: string;
+  agencia?: string;
+  conta?: string;
+  tipoConta?: "Conta Corrente" | "Poupança";
+}
+
 export interface DadosModel {
   id?: string;
   etapaAtual?: number;
+  whatsappEnviado?: boolean;
 
   dadosPessoais?: DadosPessoaisForm;
   detalhesPedido?: DetalhesPedidoForm;
   simulacao?: SimulacaoForm;
   confirmacao?: ConfirmacaoForm;
-
-  whatsappEnviado?: boolean;
+  dadosBancarios?: DadosBancariosNModel;
 }
