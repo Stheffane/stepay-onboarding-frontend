743d57945c69578a9acb05923bce1c3ee9ac2a38
stheffane.nunes <stheffane.tr@gmail.com>
2026-04-05 19:35:14 -0300

    feat: create to new stack, remove analytics

--- src/features/onboarding/quarta-etapa/pages/SolicitacaoConcluida.tsx
+++ src/features/onboarding/quarta-etapa/pages/SolicitacaoConcluida.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import { Container, Typography, Box, Button } from "@mui/material";
+import OfflinePinOutlinedIcon from '@mui/icons-material/OfflinePinOutlined';
 
-const SolicitacaoConcluida: React.FC = () => {
+export default function SolicitacaoConcluidaPage() {
   return (
-    <div>
-      <h1>Solicitação Concluída</h1>
-      {/* Conteúdo da página */}
-    </div>
-  );
-};
+    <Container maxWidth="sm">
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        textAlign="center"
+        pt={6}
+        gap={2}
+      >
+        <OfflinePinOutlinedIcon
+          sx={{ fontSize: 80, color: "success.main" }}
+        />
 
-export default SolicitacaoConcluida;
+        <Typography variant="h4">
+          Sua solicitação de crédito foi concluída com sucesso!
+        </Typography>
+
+        <Typography variant="h6" color="text.secondary">
+          Você pode acompanhar o status da sua solicitação dentro da plataforma,
+          no menu: "Meus empréstimos".
+        </Typography>
+
+        <Button
+          variant="contained"
+          size="large"
+          href="https://github.com/Stheffane/stepay-onboarding-frontend"
+          sx={{ mt: 2 }}
+        >
+          Acessar Minha Área
+        </Button>
+      </Box>
+    </Container>
+  );
+}
