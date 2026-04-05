fa9c1b9682c6bd699836b8d5c3f3a95e2810b8ec
stheffane.nunes <stheffane.tr@gmail.com>
2026-04-05 19:34:46 -0300

    feat: create to new stack, remove analytics and contexts

--- src/features/onboarding/quarta-etapa/pages/PedidoCancelado.tsx
+++ src/features/onboarding/quarta-etapa/pages/PedidoCancelado.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import { Container, Typography, Box, Button } from "@mui/material";
+import HighlightOffOutlinedIcon from '@mui/icons-material/HighlightOffOutlined';
 
-const PedidoCancelado: React.FC = () => {
+// const ICON_URL = "/images/icon-clock-box-with-papers.svg";
+
+export default function PedidoCanceladoPage() {
   return (
-    <div>
-      <h1>Pedido Cancelado</h1>
-      {/* Conteúdo da página */}
-    </div>
-  );
-};
+    <Container maxWidth="sm">
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        textAlign="center"
+        pt={6}
+        gap={2}
+      >
+        <HighlightOffOutlinedIcon
+          sx={{ fontSize: 80, color: "secondary.main" }}
+        />
 
-export default PedidoCancelado;
+        <Typography variant="h4" fontWeight={800}>
+          Oops!!
+        </Typography>
+
+        <Typography variant="h4">
+          Seu pedido foi cancelado!
+        </Typography>
+
+        <Typography variant="h6" color="text.secondary">
+          Caso um dos sócios da sua empresa tenha desistido da solicitação ou
+          sido reprovado no cadastro, infelizmente não conseguimos prosseguir
+          com o seu pedido de empréstimo. Esperamos vê-lo em breve. Em caso de
+          dúvidas, entre em contato conosco.
+        </Typography>
+
+        <Button
+          variant="contained"
+          size="large"
+          href="https://github.com/Stheffane/stepay-onboarding-frontend"
+          sx={{ mt: 2 }}
+        >
+          Finalizar
+        </Button>
+      </Box>
+    </Container>
+  );
+}
